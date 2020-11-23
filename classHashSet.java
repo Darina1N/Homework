1f1ab64ee1621ee7ba1c0dc21174dae26d9b57ae
Darina Kolesarova <darina.kolesarova@kosickaakademia.sk>
2020-11-23 22:41:35 +0100

    vypíše počet mien

--- classHashSet.java
+++ classHashSet.java
@@ -3,6 +3,8 @@ package sk.kosickaakademia.kolesarova.homework;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,12 +16,24 @@ public class classHashset {
             BufferedReader reader=new BufferedReader(fileReader);
 
             String line;
+            int pocet=0;
             while((line= reader.readLine())!=null){
                 names.add(line);
                 }
             for(String temp:names){
                 System.out.println(temp);
+                pocet++;
             }
+            System.out.println("Pocet mien je: "+pocet);
+           /* ArrayList<String> zoznam=new ArrayList<>();
+            zoznam.add(String.valueOf(names));
+            for(int i=0;i< zoznam.size();i++) {
+
+                Collections.sort(zoznam);
+            }
+            for(String temp:zoznam){
+                System.out.println(temp);
+            }*/
             fileReader.close();
         }catch (IOException exception){
             exception.printStackTrace();
